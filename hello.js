77f603541e30a87e939ae9742de6b10a237bdc1a
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-23 03:07:32 +0930

    Add challenge: Array vs String includes logic puzzle

--- hello.js
+++ hello.js
@@ -1,5 +1,4 @@
-// wht will be the output of console.log(a)?
-
+// 1. wht will be the output of console.log(a)?
 function one(){
     return 2
 }
@@ -12,4 +11,17 @@ let a =(one(), two())
 
 
 console.log(a);
+
+// 2. whht will be the output
+const arr=['one', 'two', 'three']
+const str="hello"
+
+const res= arr.includes('on')
+const anotherVar = arr.includes('onetwo')
+
+const newVar= str.includes('ll')
+
+// output will be (flase, flase, true) because arr.includes checks if the 'on' is exaclty as element in array arr. 
+// and str.incldes('ll') if there is a substring in the string which is sequetially as 'll'
+console.log(res, anotherVar, newVar)
 
