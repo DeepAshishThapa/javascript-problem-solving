13b39021aca0bd9dc103409a296781eed9b0d7e4
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-23 02:55:16 +0930

    Add challenge: Comma operator evaluation puzzle

--- hello.js
+++ hello.js
@@ -1,2 +1,15 @@
-const x=1
-console.log(x)
+// wht will be the output of console.log(a)?
+
+function one(){
+    return 2
+}
+
+function two(){
+    return 4
+}
+// a is assigned as 4 because first it is assigned as 2 when one() runs then let can be resiggned so it becomes 4 at last
+let a =(one(), two())
+
+
+console.log(a);
+
