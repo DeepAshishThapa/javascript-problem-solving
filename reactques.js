c445a47b3ae37f5c31b5bf4f9edd36e7e16cc09a
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-25 11:43:24 +0930

    docs: add Angular vs React comparison and npm/node_modules overview

--- reactques.js
+++ reactques.js
@@ -4,3 +4,16 @@
     // 2. Is react library or framework?
     // React is a library built on top of js. Its library because its provides more control to the user and donot 
     // have strict folder structure built in rules like frameworks like nextjs.
+
+    // 3. Whts the difference in angular and react?
+    // React is library and angular is a complete framework. That means react provides more control and freedom abt folder
+    // structure, routing while angular has strict folder structure and has built in rules and react is also morelightweight 
+    // compared to angular.
+    
+    // 4. what is the rule of node modules folder.
+    // node module is generated code, so we should never touch and commit it. Node module folder is a place where all 
+    // our installed external dependencies and libraries are stored for our project. While npm is node package manager which 
+    // is default package manager for js runtime environment , npm helps to instal and uninstall all the external dependencies and 
+    // libraries to our project in the node modules folder. 
+
+    
