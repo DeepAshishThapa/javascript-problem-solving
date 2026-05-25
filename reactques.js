5bfb3307c5cd1e6a2aad07565042f867092e930a
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-05-25 10:50:34 +0930

    docs: add definition of React and explain why it is a library

---
+++ reactques.js
@@ -0,0 +1,6 @@
+    // 1. What is react?
+    // React is a library built on top of js for creating UI built by meta.
+
+    // 2. Is react library or framework?
+    // React is a library built on top of js. Its library because its provides more control to the user and donot 
+    // have strict folder structure built in rules like frameworks like nextjs.
