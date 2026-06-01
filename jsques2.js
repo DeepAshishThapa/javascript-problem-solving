dc114fd3a7d59e3b3db92b66654ab2d2a76d26bc
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-06-02 01:12:35 +0930

    docs: add JS review questions for typeof, spread, parseInt, and map

---
+++ jsques2.js
@@ -0,0 +1,20 @@
+// 1. wht eill be the output
+console.log(typeof typeof 100)
+
+//2. wht will be the output
+const arr=[..."hello"]
+console.log(arr)
+
+// 3. 
+console.log(parseInt('10+2') )
+console.log(parseInt('7FM') )
+console.log(parseInt('M7F') )  
+
+//4. 
+console.log([1,2].map((num)=>{
+    if (num>0) return;
+    return num *2
+}))
+
+
+ 
