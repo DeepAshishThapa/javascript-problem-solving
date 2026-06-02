ce16aeaea082429b48f8bd0d64394776b1e743eb
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-06-02 18:26:21 +0930

    docs: explain slice vs splice and demonstrate array .find()

--- jsques2.js
+++ jsques2.js
@@ -16,5 +16,14 @@ console.log([1,2].map((num)=>{
     return num *2
 }))
 
+// 5. slice is a built in function in js used to extract a section of array or string and returns new array or string without
+// modifying the original data. While splice is built in function used to add, remove or replace items inside existing array.
+
+// 6.
+const arr2= [8,2,3,5]
+
+const findthevalue = arr2.find((value)=>value>4)
+
+console.log(findthevalue)
 
  
