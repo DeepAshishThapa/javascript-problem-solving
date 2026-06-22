0f655e10406f567b32c1bddfc70380ba04172beb
DeepAshishThapa <dipashishthapa98@gmail.com>
2026-06-22 22:35:11 +0930

    add complete sentence reversal and palindrome checker

--- jsques3.js
+++ jsques3.js
@@ -18,3 +18,32 @@ function reverseSentence(sentence){
 }
 
 console.log(reverseSentence("I am here"))
+
+// 3 to reverse entire sentence
+function reverseSentence2(str){
+    reversedsentence=""
+    for (let char of str){
+        reversedsentence = char + reversedsentence
+    }
+    return reversedsentence
+}
+
+console.log(reverseSentence2("I am here"))
+
+// 3 check if the string is palindrom or not
+function isPalindrome(str){
+    let reversed = str.split('').reverse().join('')
+
+    return reversed === str
+}
+
+console.log(isPalindrome("racecar"))
+
+// comparing the two arrays, 
+let arr1 = [100, 200, 300, 400]
+let arr2 = [100, 200, 300, 400]
+
+
+
+
+
